c4a72d4973da6170105b99f3af92192e22dd837c
CalvinRA <97717845+CalvinRA@users.noreply.github.com>
2022-05-27 22:33:36 -0600

    Create ClassExample.cs

---
+++ ClassExample.cs
@@ -0,0 +1,25 @@
+//DGM 1610 Calvin Ambrose
+//Nothing realy happens when I run this program, but I assume the point is to add things anyway
+
+public class ClassExample
+{
+    
+}
+
+public class ClassWithVariables
+{
+    public int number = 2;
+	public int speed = 7;
+    public string userName = "Bob";
+	public string color = "Green";
+}
+
+public class ClassWithFunctions
+{
+    public void RunFunction()
+    {
+        Console.WriteLine("Running Function");
+		Console.WriteLine("Fullfiling Purpose");
+		Console.WriteLine("Awaiting Consiquences");
+    }
+}
