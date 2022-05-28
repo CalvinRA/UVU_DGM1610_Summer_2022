d99e8f04a0d6c761189ba0937e93f2b43c420d5e
CalvinRA <97717845+CalvinRA@users.noreply.github.com>
2022-05-27 22:19:47 -0600

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+//DGM 1610 Calvin Ambrose
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        //Change "HW" to "Hello World"
+        Console.WriteLine("Hello World");
+    }    
+}
