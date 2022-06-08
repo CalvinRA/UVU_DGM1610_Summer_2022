e3a539a2457e0e3377639ea1350329a46f9943ce
CalvinRA <97717845+CalvinRA@users.noreply.github.com>
2022-06-08 16:42:12 -0600

    Create Lab3PracticeWhileLoops.cs

---
+++ Lab3PracticeWhileLoops.cs
@@ -0,0 +1,30 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		int i = 1;
+		
+		while(i <= 7)
+		{
+			Console.WriteLine(i);
+			i++;
+		}
+		
+		while(i >= 4)
+		{
+			Console.WriteLine(i);
+			i = i - 2;
+		}
+		
+		do
+		{
+			Console.WriteLine(i);
+			i = i * 3;
+		}
+		while(i >= 10);
+		
+		Console.WriteLine(i);
+	}
+}
