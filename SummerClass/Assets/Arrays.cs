bbc71af0393e40de7663f3ea31d781198fcb8f7a
CalvinRA <10659444@my.uvu.edu>
2022-06-23 23:23:16 -0600

    Created EnumScript

--- SummerClass/Assets/Arrays.cs
+++ SummerClass/Assets/Arrays.cs
@@ -11,9 +11,9 @@ public class Arrays : MonoBehaviour
     {
         players = GameObject.FindGameObjectsWithTag("Player");
 
-        for(int i = 0; i < players.length; i++)
+        for(int i = 0; i < players.Length; i++)
         {
-            Debug.Log("Player Number " + i + " is named " + player[i].name);
+            Debug.Log("Player Number " + i + " is named " + players[i].name);
         }
     }
 
